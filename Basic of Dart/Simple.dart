f6497b5590835b0e2b78ee2c292a2c44ba989da5
Clear20-22 <sojib1472004@gmail.com>
2025-05-16 23:14:20 +0600

    change commit

---
+++ Basic of Dart/Simple.dart
@@ -0,0 +1,24 @@
+void main() {
+  print("Hello World!");
+
+  String s = "Sojib";
+  print(s);
+
+  int roll = 23;
+  print(roll);
+
+  bool isRaining = true;
+  print(isRaining);
+
+  dynamic addr = "Du";
+  print(addr);
+
+  fun();
+
+  List fullName = ["Jubayer", "Ahmed", "Sojib"];
+  print(fullName);
+}
+
+void fun() {
+  print("This is function.");
+}
